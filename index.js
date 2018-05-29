4fff06221c67174cd11555efc753c17084a90900
SuperJesseH <therealjhorwitz@gmail.com>
2018-05-29 15:32:12 -0400

    added pin function, geocoder and auto zoom

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
 document.addEventListener("DOMContentLoaded", function(event) {
 
 const myLocButton = document.getElementById('my-location-button')
+const myAddressSearchBox = document.getElementById('primary-input')
 
 let myLat
 let myLon
 let origins = {}
+var markers = new L.featureGroup([]);
+
 
 myLocButton.addEventListener('click', (e)=>{
   // get lat and lon of user
@@ -17,9 +20,29 @@ myLocButton.addEventListener('click', (e)=>{
 
 })
 
+myAddressSearchBox.addEventListener('keydown', (e)=>{
+  if (e.key === 'Enter'){
+    const addressQuery = e.target.value
+    const myMapBoundries = mymap.getBounds()
+     const uri = `https://nominatim.openstreetmap.org/?format=json&addressdetails=1&q=${encodeURIComponent(addressQuery)}&countrycodes=us&viewbox=${mymap.getBounds()._northEast.lat},${mymap.getBounds()._northEast.lng},${mymap.getBounds()._southWest.lat},${mymap.getBounds()._southWest.lng}&format=json&limit=1`
+
+     fetch(uri).then(json=>json.json()).then(json=>placePin([json[0].lat,json[0].lon], json[0].display_name));
+
+  };
+})
+
+
+function getPlaceFromAddress(addressString) {
+
+}
+
+
 function placePin(coordsArray, popuptext) {
-  let newMarker = new L.marker(coordsArray, title = 'Hello').addTo(mymap);
-  newMarker.bindPopup(`${popuptext}`).openPopup();
+  console.log(coordsArray, popuptext)
+  let newMarker = new L.marker(coordsArray).addTo(mymap);
+  newMarker.addTo(markers)
+  newMarker.bindPopup(`${popuptext}`);
+  mymap.fitBounds(markers.getBounds())
 }
 
 
@@ -32,4 +55,5 @@ L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token=p
     'Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
   id: 'mapbox.streets'
 }).addTo(mymap);
+// L.Control.geocoder().addTo(mymap);
 })
